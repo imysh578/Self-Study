c81f8feeff73e3a0dfefc2ad16212ef9590f35f2
imysh578 <33863016+imysh578@users.noreply.github.com>
2021-12-07 17:27:28 +0900

    hello world study

---
+++ go/1.HelloWorld/hello.go
@@ -0,0 +1,17 @@
+package main
+
+/*
+	main : program start point
+	main package should be only one in the program
+*/
+
+import "fmt"
+
+// import : using other packages
+// cannot import main package
+// fmt :
+
+func main() {
+	// func : function, functional unit
+	fmt.Println("Hello World")
+}
