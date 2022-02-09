533a4365e0d2fcba55b557f3b54c9e64b0a80bad
imysh578 <33863016+imysh578@users.noreply.github.com>
2022-02-10 00:07:57 +0900

    Add Solidity folder and create zombieFactory.sol

---
+++ Solidity/CryptoZombie/zombiefactory.sol
@@ -0,0 +1,26 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+  event NewZombie(uint id, string name, uint dna);
+
+  uint dnaDigits = 16;
+  uint dnaModulus = 10 ** dnaDigits;
+
+  struct Zombie {
+    string name;
+    uint dna;
+  }
+
+  Zombie[] public zombies;
+  
+  function _createZombie (string memory _name, uint _dna) private {
+    uint id = zombies.push(Zombie(_name, _dna)) - 1;
+    emit NewZombie(id, _name, _dna);
+  }
+
+  function _generateRandomDna (string memory _name) private view returns (uint) {
+    uint rand = uint(keccak256(abi.encodePacked(_name)));
+    return rand % dnaModulus;
+  }
+}
