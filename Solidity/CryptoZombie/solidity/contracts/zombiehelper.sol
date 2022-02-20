651b2828562ef4570d2a64d7e183fe86eeea952b
imysh578 <33863016+imysh578@users.noreply.github.com>
2022-02-20 20:46:36 +0900

    chapter 1, 2 finished

--- Solidity/CryptoZombie/solidity/contracts/zombiehelper.sol
+++ Solidity/CryptoZombie/solidity/contracts/zombiehelper.sol
@@ -3,10 +3,29 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
+  uint levelUpFee = 0.001 ether;
+
   // 좀비가 지정된 레벨보다 높거나 같은지 확인
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
+  }
+
+  // 일정 이더를 지불하면 레벨업하는 함수
+  function levelUp(uint _zombieId) external payable {
+    require(msg.value == levelUpFee); // 지불 금액이 맞는지 확인
+    zombies[_zombieId].level++;
+  }
+
+  // 현재 컨트랙트의 잔액을 Owner에게 전송
+  function withdraw() external onlyOwner {
+    address payable _onwer = address(uint160(owner()));
+    _onwer.transfer(address(this).balance);
+  }
+
+  // 레벨업에 필요한 이더의 양 수정
+  function setLevelUpFee(uint _fee) external onlyOwner {
+    levelUpFee = _fee;
   }
 
   // 레벨 2이상이면 좀비 이름 변경 가능
@@ -33,4 +52,6 @@ contract ZombieHelper is ZombieFeeding {
     }
     return result;
   }
+
+  
 }
