e2ac4269eed0873a8235c0069cb07ab89ebe9cd7
imysh578 <33863016+imysh578@users.noreply.github.com>
2022-03-03 19:37:29 +0900

    finished chapter 3

---
+++ Solidity/CryptoZombie/solidity/contracts/zombieownership.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombieattack.sol";
+import "./erc721.sol";
+
+contract ZombieOwnership is ZombieAttack {
+  function balanceOf(address _owner) external view returns (uint256) {
+    return ownerZombieCount[_owner];
+  }
+
+  function ownerOf(uint256 _tokenId) external view returns (address) {
+    return zombieToOwner[_tokenId];
+  }
+}
