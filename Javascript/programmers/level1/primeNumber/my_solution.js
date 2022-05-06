6ee0e555915e878ad8882fc28d46817df62a5a3c
imysh578 <33863016+imysh578@users.noreply.github.com>
2022-05-07 00:44:48 +0900

    studying coding test

--- Javascript/programmers/level1/primeNumber/my_solution.js
+++ Javascript/programmers/level1/primeNumber/my_solution.js
@@ -1,27 +1,26 @@
 // 소수 만들기
 function solution(nums) {
-  let selectedSums = []
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i; j < nums.length; j++) {
-      for (let k = j; k < nums.length; k++) {
-        selectedSums.push(i+j+k)
-      }
-    }
-  }
-
+  // 소수인지 확인
   const isPrime = (num) => {
-    for (let i = 0; i*i < num; i++) {
+    for (let i = 2; i*i <= num; i++) { // 숫자의 제곱근까지만 확인하면 됨
       if (num % i === 0) return false;
     }
     return true;
   }
+  let answer = 0;
   
-  const answer = selectedSums.reduce((accu, curr) => {
-    if(isPrime(curr)) accu += curr
-    return accu
-  }, 0)
+  // 3개 숫자 고르기
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i+1; j < nums.length; j++) {
+      for (let k = j+1; k < nums.length; k++) {
+        if(isPrime(nums[i]+nums[j]+nums[k])) { // 고른 숫자의 합이 소수면
+          answer++
+        }
+      }
+    }
+  }
   
   return answer;
 }
 
-solution([1,2,3,4])
+solution([1, 2, 7, 6, 4])
