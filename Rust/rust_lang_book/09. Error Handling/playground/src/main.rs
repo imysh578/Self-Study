13e84003becb4ec0c5735ddde106d1ea64bca971
imysh578 <imysh578@gmail.com>
2023-02-16 00:42:53 +0900

    Studying 9-2

--- Rust/rust_lang_book/09. Error Handling/playground/src/main.rs
+++ Rust/rust_lang_book/09. Error Handling/playground/src/main.rs
@@ -1,5 +1,20 @@
-fn main() {
-    let v = vec![1, 2, 3];
+use std::fs::File;
+use std::io::{self, Read};
 
-    v[99];
+fn main() {
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let username_file_result = File::open("hello.txt");
+
+        let mut username_file = match username_file_result {
+            Ok(file) => file,
+            Err(e) => return Err(e),
+        };
+
+        let mut username = String::new();
+
+        match username_file.read_to_string(&mut username) {
+            Ok(_) => Ok(username),
+            Err(e) => Err(e),
+        }
+    }
 }
