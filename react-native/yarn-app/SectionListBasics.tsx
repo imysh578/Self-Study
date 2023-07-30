42d735acce4d4e59f67186ab3697eeb2c5672ba0
imysh578 <imysh578@gmail.com>
2023-07-30 23:49:20 +0900

    comments: add more descriptions

--- react-native/yarn-app/SectionListBasics.tsx
+++ react-native/yarn-app/SectionListBasics.tsx
@@ -9,9 +9,10 @@ import {
 } from "react-native";
 
 const DATA = [
+    // FlatList의 props들을 정의한 데이터들을 입력
     {
-        title: "Main dishes",
-        data: ["Pizza", "Burger", "Risotto"],
+        title: "Main dishes", // 필드명은 아무거나 해도 상관 없음 (e.g., title, label, ...)
+        data: ["Pizza", "Burger", "Risotto"], // data 필드는 필수!, Flat List의 데이터 필드와 동일한 역할
     },
     {
         title: "Sides",
@@ -30,9 +31,10 @@ const DATA = [
 const SectionListBasics = () => (
     <SafeAreaView style={styles.container}>
         <SectionList
-            sections={DATA}
+            sections={DATA} // 위에서 정의한 세션 데이터를 넣음
             keyExtractor={(item, index) => item + index}
             renderItem={({ item }) => (
+                // 출력할 아이템들을 여기서 구현 (item: DATA의 요소 중 data 필드의 하나하나의 값)
                 <View style={styles.item}>
                     <Text style={styles.title}>{item}</Text>
                 </View>
