dfd598431a50eeab2c4bcf0a6899c42383b7484f
imysh578 <imysh578@gmail.com>
2024-06-09 23:03:56 +0900

    Add git ignore

--- Rust/playground/target/debug/deps/playground-3de265ac3bd2f95d.d
+++
@@ -1,5 +0,0 @@
-/Users/seokhunyoon/Github/personal/Self-Study/Rust/playground/target/debug/deps/libplayground-3de265ac3bd2f95d.rmeta: src/main.rs
-
-/Users/seokhunyoon/Github/personal/Self-Study/Rust/playground/target/debug/deps/playground-3de265ac3bd2f95d.d: src/main.rs
-
-src/main.rs:
--- Rust/playground/target/debug/playground.d
+++
@@ -1,1 +0,0 @@
-/Users/seokhunyoon/Github/personal/Self-Study/Rust/playground/target/debug/playground: /Users/seokhunyoon/Github/personal/Self-Study/Rust/playground/src/main.rs
