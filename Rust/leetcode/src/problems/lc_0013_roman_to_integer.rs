70912e72c2c6b6dd1ea2cfcfe85ea2e4b7419669
imysh578 <imysh578@gmail.com>
2024-08-06 23:00:38 +0900

    Feat: add leetcode 0013 roman to integer

--- Rust/leetcode/src/problems/lc_0013_roman_to_integer.rs
+++ Rust/leetcode/src/problems/lc_0013_roman_to_integer.rs
@@ -1,3 +1,22 @@
 pub fn roman_to_int(s: String) -> i32 {
-    0
+    let mut result = 0;
+    let mut prev = 0;
+
+    for c in s.chars().rev() {
+        let current = match c {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => 0,
+        };
+
+        result += if prev > current { -current } else { current };
+        prev = current;
+    }
+
+    result
 }
