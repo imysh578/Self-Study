2cbf11a9d200b8f205ae0ce152f71e23a7f2eebe
imysh578 <imysh578@gmail.com>
2024-08-28 00:25:01 +0900

    lc0003: fix the previous commit

--- Rust/leetcode/src/problems/lc_0003_longest_substring_without_repeating_charaters.rs
+++ Rust/leetcode/src/problems/lc_0003_longest_substring_without_repeating_charaters.rs
@@ -1,3 +1,26 @@
+// pub fn length_of_longest_substring(s: String) -> i32 {
+//     use std::cmp::max;
+//     use std::collections::HashMap;
+//
+//     let mut map = HashMap::new();
+//     let mut prev = 0;
+//     let mut longest_length = 0;
+//
+//     for (curr, char) in s.chars().enumerate() {
+//         let found_index = map.get(&char);
+//
+//         while found_index.is_some() && *found_index.unwrap() >= prev {
+//             prev += 1;
+//         }
+//
+//         longest_length = max(longest_length, curr as i32 - prev + 1);
+//
+//         map.insert(char, curr as i32);
+//     }
+//
+//     longest_length
+// }
+
 pub fn length_of_longest_substring(s: String) -> i32 {
     use std::cmp::max;
     use std::collections::HashMap;
@@ -7,10 +30,8 @@ pub fn length_of_longest_substring(s: String) -> i32 {
     let mut longest_length = 0;
 
     for (curr, char) in s.chars().enumerate() {
-        let found_index = map.get(&char);
-
-        while found_index.is_some() && *found_index.unwrap() >= prev {
-            prev += 1;
+        if let Some(found_index) = map.insert(char, curr as i32) {
+            prev = max(prev, found_index);
         }
 
         longest_length = max(longest_length, curr as i32 - prev);
