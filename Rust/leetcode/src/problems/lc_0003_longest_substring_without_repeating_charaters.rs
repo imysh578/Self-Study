c73a4b0ac02ac27a24924ef46f1bea2226577c67
imysh578 <imysh578@gmail.com>
2024-08-27 23:58:57 +0900

    lc0003: resolved it with two pointer, prev and curr

--- Rust/leetcode/src/problems/lc_0003_longest_substring_without_repeating_charaters.rs
+++ Rust/leetcode/src/problems/lc_0003_longest_substring_without_repeating_charaters.rs
@@ -1,27 +1,21 @@
 pub fn length_of_longest_substring(s: String) -> i32 {
+    use std::cmp::max;
     use std::collections::HashMap;
 
-    let mut current_map = HashMap::new();
+    let mut map = HashMap::new();
+    let mut prev = 0;
     let mut longest_length = 0;
 
-    for (index, char) in s.chars().enumerate() {
-        match current_map.get(&char) {
-            None => {
-                current_map.insert(char, index as i32);
-            }
-            Some(&found_index) => {
-                current_map.clear();
+    for (curr, char) in s.chars().enumerate() {
+        let found_index = map.get(&char);
 
-                for val in found_index + 1..=index as i32 {
-                    println!("{}", val);
-                    current_map.insert(s.chars().nth(val as usize).unwrap(), val);
-                }
-            }
+        while found_index.is_some() && *found_index.unwrap() >= prev {
+            prev += 1;
         }
 
-        if longest_length < current_map.len() as i32 {
-            longest_length = current_map.len() as i32;
-        }
+        longest_length = max(longest_length, curr as i32 - prev + 1);
+
+        map.insert(char, curr as i32);
     }
 
     longest_length
