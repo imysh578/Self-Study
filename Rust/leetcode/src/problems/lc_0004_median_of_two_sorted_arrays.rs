676a27577ecf88df681f07cd8728ea8d3e0c22a8
imysh578 <imysh578@gmail.com>
2024-09-24 23:18:21 +0900

    lc0004: remove unused inital value of result

--- Rust/leetcode/src/problems/lc_0004_median_of_two_sorted_arrays.rs
+++ Rust/leetcode/src/problems/lc_0004_median_of_two_sorted_arrays.rs
@@ -26,7 +26,7 @@ pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
     let remainder_by_two = merged_array.len() % 2;
     let is_even_len = remainder_by_two == 0;
 
-    let mut result = 0.0;
+    let result: f64;
     if is_even_len {
         result =
             (merged_array[quotient_by_two - 1] as f64 + merged_array[quotient_by_two] as f64) / 2.0;
