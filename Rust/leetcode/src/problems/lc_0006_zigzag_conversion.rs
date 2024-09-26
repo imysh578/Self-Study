9f4b6d73bca1a9db2b476be9510f93ac5454df82
imysh578 <imysh578@gmail.com>
2024-09-26 21:47:51 +0900

    lc0006: resolved it

--- Rust/leetcode/src/problems/lc_0006_zigzag_conversion.rs
+++ Rust/leetcode/src/problems/lc_0006_zigzag_conversion.rs
@@ -1,1 +1,36 @@
-pub fn convert(s: String, num_rows: i32) -> String {}
+pub fn convert(s: String, num_rows: i32) -> String {
+    if num_rows == 1 || (s.len() as i32) <= num_rows {
+        return s;
+    }
+
+    let mut rows: Vec<String> = vec![String::new(); num_rows as usize];
+    let mut current_row = 0;
+
+    enum Direction {
+        Down,
+        Up,
+    }
+    let mut current_direction = Direction::Down;
+
+    for c in s.chars() {
+        rows[current_row].push(c);
+
+        match current_direction {
+            Direction::Down => {
+                current_row += 1;
+            }
+            Direction::Up => {
+                current_row -= 1;
+            }
+        }
+
+        if current_row >= (num_rows - 1) as usize {
+            current_direction = Direction::Up;
+        }
+        if current_row <= 0 as usize {
+            current_direction = Direction::Down;
+        }
+    }
+
+    rows.concat()
+}
